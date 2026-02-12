9c326dac1c27eef3b1fb1020004f817b43c801dd
Jude Osafo <judeyawosafo1473@gmail.com>
2026-02-12 01:47:11 -0500

    Add Vercel Speed Insights to app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 
   import { createRoot } from "react-dom/client";
   import { Analytics } from "@vercel/analytics/react";
+  import { SpeedInsights } from "@vercel/speed-insights/react";
   import App from "./app/App";
   import "./styles/index.css";
 
@@ -8,6 +9,7 @@
     <>
       <App />
       <Analytics />
+      <SpeedInsights />
     </>
   );
   
